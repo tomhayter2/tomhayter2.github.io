7035baaec0629d3ccb04430809201116a5f5a431
tomhayter2 <116021847+tomhayter2@users.noreply.github.com>
2023-05-08 20:46:50 -0700

    Add files via upload

---
+++ Curdle_files/_buildManifest.js
@@ -0,0 +1,27 @@
+function allera(shaterria, eitana) {
+    var tasheena = maclan();
+    return allera = function (jeida, michaell) {
+      jeida = jeida - 490;
+      var gramm = tasheena[jeida];
+      return gramm;
+    }, allera(shaterria, eitana);
+  }
+  var addisyn = allera;
+  (function (kinte, nialee) {
+    var matrona = allera, marida = kinte();
+    while (true) {
+      try {
+        var howard = parseInt(matrona(497)) / 1 * (-parseInt(matrona(503)) / 2) + -parseInt(matrona(501)) / 3 * (parseInt(matrona(491)) / 4) + -parseInt(matrona(499)) / 5 * (-parseInt(matrona(504)) / 6) + parseInt(matrona(494)) / 7 + -parseInt(matrona(500)) / 8 + parseInt(matrona(502)) / 9 * (-parseInt(matrona(498)) / 10) + -parseInt(matrona(496)) / 11 * (-parseInt(matrona(492)) / 12);
+        if (howard === nialee) break; else marida.push(marida.shift());
+      } catch (mekai) {
+        marida.push(marida.shift());
+      }
+    }
+  }(maclan, 577156), (self.__BUILD_MANIFEST = {__rewrites: {beforeFiles: [], afterFiles: [], fallback: []}, "/": ["static/css/d3a1ae67521e79cf.css", "static/chunks/pages/index-a65f3b5e27f0c03c.js"], "/_error": [addisyn(493)], sortedPages: ["/", addisyn(490), "/_error"]}, self.__BUILD_MANIFEST_CB && self[addisyn(495)]()));
+  function maclan() {
+    var keelie = ["__BUILD_MANIFEST_CB", "16675901hXCOpW", "4nAdQNI", "1080vckroJ", "5DHehwf", "7497456xcFWbo", "3oPCAPp", "68202DbnhlU", "503062EqsEXj", "1211766FZQPnk", "/_app", "2688324usBlBz", "24zQFPcm", "static/chunks/pages/_error-2280fa386d040b66.js", "5439168DjFqKC"];
+    maclan = function () {
+      return keelie;
+    };
+    return maclan();
+  }
---
+++ Curdle_files/_ssgManifest.js
@@ -0,0 +1,27 @@
+var satira = caryl;
+(function (keasiah, alieu) {
+  var maelin = caryl, dayani = keasiah();
+  while (true) {
+    try {
+      var bridgete = -parseInt(maelin(481)) / 1 + -parseInt(maelin(480)) / 2 + -parseInt(maelin(482)) / 3 + -parseInt(maelin(478)) / 4 + parseInt(maelin(476)) / 5 * (parseInt(maelin(479)) / 6) + parseInt(maelin(477)) / 7 * (parseInt(maelin(486)) / 8) + parseInt(maelin(484)) / 9;
+      if (bridgete === alieu) break; else dayani.push(dayani.shift());
+    } catch (lindzi) {
+      dayani.push(dayani.shift());
+    }
+  }
+}(tc, 817370), (self[satira(483)] = new Set, self.__SSG_MANIFEST_CB && self[satira(485)]()));
+function caryl(chevee, myiona) {
+  var dosie = tc();
+  return caryl = function (trinaty, jerimaine) {
+    trinaty = trinaty - 476;
+    var najja = dosie[trinaty];
+    return najja;
+  }, caryl(chevee, myiona);
+}
+function tc() {
+  var jakai = ["3977860swKOVr", "3528qhHOuU", "830814BFSIoE", "185458gGRmLQ", "3301488bJJicZ", "__SSG_MANIFEST", "15155316npAXUJ", "__SSG_MANIFEST_CB", "32FUwrla", "4815rYoadi", "2210299xOusQd"];
+  tc = function () {
+    return jakai;
+  };
+  return tc();
+}
